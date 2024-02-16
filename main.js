82d740a0d10a637b1d0837cc6032458f2d58d322
meherab-mhk <meherabh43@gmail.com>
2024-02-16 12:02:08 +0600

    rectangle formula added

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
+//triangle
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const triBaseInput = document.getElementById('tri-base-input').value;
     const triHeightInput = document.getElementById('tri-height-input').value;
     const resultDisplay = document.getElementById('result-display');
-    const triangBase = parseFloat(triBaseInput);
+    const triangleBase = parseFloat(triBaseInput);
     const triangleHeight = parseFloat(triHeightInput);
-    const area = 0.5 * triangBase * triangleHeight;
-    resultDisplay.innerText = "Area is: "+ area.toFixed(3) + " m2";
+    const area = 0.5 * triangleBase * triangleHeight;
+    resultDisplay.innerText = "Area is: "+ area + " m2";
 });
+//rectangle
+document.getElementById('rect-calculate-btn').addEventListener('click', function(){
+    const rectWidthInput = document.getElementById('rect-width-input').value;
+    const rectLengthInput = document.getElementById('rect-length-input').value;
+    const resultDisplay = document.getElementById('rect-result-display');
+    const rectWidth = parseFloat(rectWidthInput);
+    const rectlength = parseFloat(rectLengthInput);
+    const area = rectWidth * rectlength;
+    resultDisplay.innerText = "Area is: "+ area + " m2";
+});
+
 
